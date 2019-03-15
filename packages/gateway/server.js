39a859c3a8944ac3501e2f10686a1cd89194c8e5
Antonio1027 <shilong_92@hotmail.com>
2019-03-14 23:15:38 -0600

    feature #1 graphql installation

---
+++ packages/gateway/server.js
@@ -0,0 +1,25 @@
+const server = require('express')();
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const bodyParser = require('body-parser');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const typeDefs = `
+    type Query { hey: String! }
+`;
+
+const resolvers = {
+    Query: {
+        hey: () => 'hey there'
+    }
+};
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
+
+server
+    .use(bodyParser.json())
+    .use('/graphql', graphqlExpress({ schema }))
+    .use('/gq', graphiqlExpress({ endpointURL: '/graphql' }))
+    .listen(3000, () => console.log('Listening'));

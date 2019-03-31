0b58fa2ce34f1f5adc40d2563a9a22fddb097e04
Antonio1027 <shilong_92@hotmail.com>
2019-03-31 12:04:19 -0600

    feature #1 Resolvers

--- packages/gateway/data/resolvers.js
+++ packages/gateway/data/resolvers.js
@@ -1,5 +1,30 @@
+const mockLocations = [
+    {
+        latitude: '12.564645656',
+        longitude: '34.654645654',
+        emai: 'REOUR8989DF890'
+    },
+    {
+        latitude: '15.564645656',
+        longitude: '23.654645654',
+        emai: 'REOURHFRT89DF890'
+    },
+    {
+        latitude: '11.564645656',
+        longitude: '74.654645654',
+        emai: 'REOUR8989DFGGFG'
+    }
+];
+
 module.exports = {
     Query: {
-        hey: () => 'hey you !!'
+        locations: () => mockLocations,
+        location: (_, args) => mockLocations[0]
+    },
+    Mutation: {
+        location: (_, args) => {
+            mockLocations[0] = args;
+            return args;
+        }
     }
-}
+};
--- packages/gateway/data/schema.js
+++ packages/gateway/data/schema.js
@@ -2,7 +2,21 @@ const { makeExecutableSchema } = require('graphql-tools');
 const resolvers = require('./resolvers');
 
 const typeDefs = `
-    type Query { hey: String! }
+    type Query {
+        locations: [Location]
+        location(latitude: String!, longitude: String!): Location
+    }
+
+    type Mutation {
+        location(latitude: String!, longitude: String!, emai: String!): Location
+    }
+
+    type Location {
+        latitude: String
+        longitude: String
+        emai: String
+        _id: String
+    }
 `;
 
 module.exports = makeExecutableSchema({ typeDefs , resolvers });
